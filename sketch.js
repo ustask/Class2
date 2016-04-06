4893eff6d1881956b23e737ceddf3d2b5231f8bb
ustask <kathleenustas@Kathleens-MacBook-Pro.local>
2016-04-05 22:05:15 -0400

    Call attempt

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
-function setup () {
-  createCanvas(600,600);
-}
+ //Assignment 2 (#4)
+ // Create an object that describes you as an object var you = {}; or var you = function() {};
+ // Create another object that describes your favorite food. use var foodName = {} where foodName is the name of the food.
+ // Assign your favorite food to be a property of that object that describes you
+ // Print out an attribute from your object using either console.log or P5's text() function.
+ // Create an array and then use a loop to print out or modify every value
 
-function draw() {
 
-}
+
+var me = {name: 'Kathleen', favoriteFood:'sushi'};
+
+console.log(me.favoriteFood);
